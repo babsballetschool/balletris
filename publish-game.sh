924706377a23208e16023cb1712e4131ff982ae4
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-08-26 13:41:14 +0200

    Publish game script

---
+++ publish-game.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env bash
+
+set -o errexit -o nounset
+
+if [ -f ~/.bash_tokens ]; then
+    source ~/.bash_tokens
+fi
+
+rev=$(git rev-parse --short HEAD)
+
+make all
+
+cd build
+
+git init
+git config user.name "Daan van Berkel"
+git config user.email "daan@fifth-postulate.nl"
+
+git remote add upstream "https://$GH_TOKEN@github.com/dvberkel/balletris.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+touch .
+touch .nojekyll
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
